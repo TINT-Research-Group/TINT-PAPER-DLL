48b6af3f4e476a8924f18866d68ba0cfb98c7964
Shiwen Liu <aprildiana005@gmail.com>
2026-08-31 21:09:56 +0800

    Add conference deadline type filter

--- static/js/main.js
+++ static/js/main.js
@@ -1,157 +1,194 @@
 ---
 ---
 $(function() {
-  deadlineByConf = {};
+  var deadlineTypes = {
+    abstract: { label: 'Abstract' },
+    full_paper: { label: 'Full Paper' },
+    first_round_decision: { label: 'First-round Decision' }
+  };
+  var conferenceDates = {};
+  var deadlineByConf = {};
 
   {% for conf in site.data.conferences %}
-  // {{ conf.name }} {{ conf.year }}
-  {% if conf.deadline[0] == "TBA" %}
-  {% assign conf_id = conf.name | append: conf.year | append: '-0' | slugify %}
-  $('#{{ conf_id }} .timer').html("TBA");
-  $('#{{ conf_id }} .deadline-time').html("TBA");
-  deadlineByConf["{{ conf_id }}"] = null;
-
-  {% else %}
-  var rawDeadlines = {{ conf.deadline | jsonify }} || [];
-  if (rawDeadlines.constructor !== Array) {
-    rawDeadlines = [rawDeadlines];
-  }
-  var parsedDeadlines = [];
-  while (rawDeadlines.length > 0) {
-    var rawDeadline = rawDeadlines.pop();
-    // deal with year template in deadline
-    year = {{ conf.year }};
-    rawDeadline = rawDeadline.replace('%y', year).replace('%Y', year - 1);
-    // adjust date according to deadline timezone
-    {% if conf.timezone %}
-    var deadline = moment.tz(rawDeadline, "{{ conf.timezone }}");
-    {% else %}
-    var deadline = moment.tz(rawDeadline, "Etc/GMT+12"); // Anywhere on Earth
-    {% endif %}
-
-    // post-process date
-    if (deadline.minutes() === 0) {
-      deadline.subtract(1, 'seconds');
-    }
-    if (deadline.minutes() === 59) {
-      deadline.seconds(59);
-    }
-    parsedDeadlines.push(deadline);
-  }
-  // due to pop before; we need to reverse such that the i index later matches
-  // the right parsed deadline
-  parsedDeadlines.reverse();
-
-  {% assign range_end = conf.deadline.size | minus: 1 %}
+  {% assign num_deadlines = conf.deadline.size %}
+  {% if num_deadlines == 0 %}
+    {% assign num_deadlines = 1 %}
+  {% endif %}
+  {% assign range_end = num_deadlines | minus: 1 %}
   {% for i in (0..range_end) %}
   {% assign conf_id = conf.name | append: conf.year | append: '-' | append: i | slugify %}
-  var deadlineId = {{ i }};
-  if (deadlineId < parsedDeadlines.length) {
-    var confDeadline = parsedDeadlines[deadlineId];
+  conferenceDates["{{ conf_id }}"] = {
+    round: {{ i | plus: 1 }},
+    roundCount: {{ num_deadlines }},
+    timezone: {{ conf.timezone | default: 'Etc/GMT+12' | jsonify }},
+    abstract: {{ conf.abstract_deadline[i] | jsonify }},
+    full_paper: {{ conf.full_paper_deadline[i] | jsonify }},
+    first_round_decision: {{ conf.first_round_decision[i] | jsonify }}
+  };
+  {% endfor %}
+  {% endfor %}
 
-    // render countdown timer
-    if (confDeadline) {
-      function make_update_countdown_fn(confDeadline) {
-        return function(event) {
-          diff = moment() - confDeadline
-          if (diff <= 0) {
-             $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
-          } else {
-            $(this).html(confDeadline.fromNow());
-          }
+  var confs = $('.conf');
+  var dateTypeStoreKey = '{{ site.domain }}-deadline-type';
+  var selectedDeadlineType = store.get(dateTypeStoreKey);
+  if (!deadlineTypes[selectedDeadlineType]) {
+    selectedDeadlineType = 'abstract';
+  }
+
+  function parseConferenceDate(rawDate, timezone) {
+    if (!rawDate || rawDate === 'TBA' || rawDate === 'N/A') {
+      return null;
+    }
+
+    var parsed = moment.tz(rawDate, timezone || 'Etc/GMT+12');
+    return parsed.isValid() ? parsed : null;
+  }
+
+  function stopCountdown(timer) {
+    try {
+      timer.countdown('remove');
+    } catch (_) {
+      // The element has no active countdown yet.
+    }
+    timer.empty();
+  }
+
+  function makeUpdateCountdown(confDeadline) {
+    return function(event) {
+      if (moment().diff(confDeadline) <= 0) {
+        $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
+      } else {
+        $(this).html(confDeadline.fromNow());
+      }
+    };
+  }
+
+  function reorderConferences() {
+    var now = moment();
+    var ordered = confs.detach().sort(function(a, b) {
+      var aDeadline = deadlineByConf[a.id];
+      var bDeadline = deadlineByConf[b.id];
+
+      if (!aDeadline && !bDeadline) return 0;
+      if (!aDeadline) return 1;
+      if (!bDeadline) return -1;
+
+      var aPast = now.diff(aDeadline) > 0;
+      var bPast = now.diff(bDeadline) > 0;
+      if (aPast !== bPast) return aPast ? 1 : -1;
+
+      if (aPast) return bDeadline.valueOf() - aDeadline.valueOf();
+      return aDeadline.valueOf() - bDeadline.valueOf();
+    });
+
+    $('.conf-container').append(ordered);
+  }
+
+  function renderDeadlineType(deadlineType) {
+    selectedDeadlineType = deadlineType;
+    deadlineByConf = {};
+
+    confs.each(function(_, element) {
+      var conf = $(element);
+      var data = conferenceDates[element.id];
+      var rawDate = data ? data[deadlineType] : null;
+      var displayValue = rawDate || 'TBA';
+      var timer = conf.find('.timer');
+      var deadline = parseConferenceDate(rawDate, data && data.timezone);
+      var label = deadlineTypes[deadlineType].label;
+
+      stopCountdown(timer);
+      conf.removeClass('past');
+
+      if (data && data.roundCount >= 2) {
+        label += ' (' + data.round + ' / ' + data.roundCount + ')';
+      }
+      conf.find('.deadline-label').text(label + ':');
+
+      if (deadline) {
+        timer.countdown(deadline.toDate(), makeUpdateCountdown(deadline));
+        conf.find('.deadline-time').text(deadline.local().format('D MMM YYYY, h:mm:ss a'));
+        deadlineByConf[element.id] = deadline;
+
+        if (moment().diff(deadline) > 0) {
+          conf.addClass('past');
         }
+      } else {
+        timer.text(displayValue);
+        conf.find('.deadline-time').text(displayValue);
+        deadlineByConf[element.id] = null;
       }
-      $('#{{ conf_id }} .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
-      // check if date has passed, add 'past' class to it
-      if (moment() - confDeadline > 0) {
-        $('#{{ conf_id }}').addClass('past');
-      }
-      $('#{{ conf_id }} .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
-      deadlineByConf["{{ conf_id }}"] = confDeadline;
-    }
-  } else {
-    // TODO: hide the conf_id ?
-  }
-  {% endfor %}
-  {% endif %}
-  {% endfor %}
+    });
 
-  // Reorder list
-  var today = moment();
-  var confs = $('.conf').detach();
-  confs.sort(function(a, b) {
-    var aDeadline = deadlineByConf[a.id];
-    var bDeadline = deadlineByConf[b.id];
-    var aDiff = today.diff(aDeadline);
-    var bDiff = today.diff(bDeadline);
-    if (aDiff < 0 && bDiff > 0) {
-      return -1;
-    }
-    if (aDiff > 0 && bDiff < 0) {
-      return 1;
-    }
-    return bDiff - aDiff;
+    $('.deadline-type-toggle')
+      .removeClass('active')
+      .attr('aria-pressed', 'false')
+      .filter('[data-deadline-type="' + deadlineType + '"]')
+      .addClass('active')
+      .attr('aria-pressed', 'true');
+
+    store.set(dateTypeStoreKey, deadlineType);
+    reorderConferences();
+  }
+
+  $('.deadline-type-toggle').on('click', function() {
+    renderDeadlineType($(this).data('deadline-type'));
+    updateConfList();
   });
-  $('.conf-container').append(confs);
 
-  // Set checkboxes
-  var conf_type_data = {{ site.data.types | jsonify }};
-  var all_tags = [];
-  var toggle_status = {};
-  for (var i = 0; i < conf_type_data.length; i++) {
-    all_tags[i] = conf_type_data[i]['tag'];
-    toggle_status[all_tags[i]] = false;
+  // Set conference tag checkboxes.
+  var confTypeData = {{ site.data.types | jsonify }};
+  var allTags = [];
+  var toggleStatus = {};
+  for (var i = 0; i < confTypeData.length; i++) {
+    allTags[i] = confTypeData[i].tag;
+    toggleStatus[allTags[i]] = false;
   }
+
   var tags = store.get('{{ site.domain }}');
   if (tags === undefined) {
-    tags = all_tags;
+    tags = allTags;
   }
-  for (var i = 0; i < tags.length; i++) {
-    $('#' + tags[i] + '-checkbox').prop('checked', false);
-    toggle_status[tags[i]] = false;
+  for (var j = 0; j < tags.length; j++) {
+    $('#' + tags[j] + '-checkbox').prop('checked', false);
+    toggleStatus[tags[j]] = false;
   }
   store.set('{{ site.domain }}', tags);
 
-  function update_conf_list() {
-    confs.each(function(i, conf) {
-      var conf = $(conf);
-      var show = false;
-      var set_tags = [];
-      for (var i = 0; i < all_tags.length; i++) {
-        // if tag has been selected by user, check if the conference has it
-        if(toggle_status[all_tags[i]]) {
-          set_tags.push(conf.hasClass(all_tags[i]));
+  function updateConfList() {
+    confs.each(function(_, element) {
+      var conf = $(element);
+      var selectedTagMatches = [];
+      for (var i = 0; i < allTags.length; i++) {
+        if (toggleStatus[allTags[i]]) {
+          selectedTagMatches.push(conf.hasClass(allTags[i]));
         }
       }
-      let empty_or_all_true = arr => arr.every(Boolean);
-      // show a conference if it has all user-selected tags
-      // if no tag is set (= array is empty), show all entries
-      show = empty_or_all_true(set_tags);
-      if (show) {
+
+      if (selectedTagMatches.every(Boolean)) {
         conf.show();
       } else {
-        conf.hide()
+        conf.hide();
       }
     });
   }
-  update_conf_list();
 
-  // Event handler on checkbox change
-  $('form :checkbox').change(function(e) {
+  $('form :checkbox').change(function() {
     var checked = $(this).is(':checked');
     var tag = $(this).prop('id').slice(0, -9);
-    toggle_status[tag] = checked;
+    toggleStatus[tag] = checked;
 
-    if (checked == true) {
-      if (tags.indexOf(tag) < 0)
-        tags.push(tag);
+    if (checked) {
+      if (tags.indexOf(tag) < 0) tags.push(tag);
+    } else {
+      var index = tags.indexOf(tag);
+      if (index >= 0) tags.splice(index, 1);
     }
-    else {
-      var idx = tags.indexOf(tag);
-      if (idx >= 0)
-        tags.splice(idx, 1);
-    }
+
     store.set('{{ site.domain }}', tags);
-    update_conf_list();
+    updateConfList();
   });
+
+  renderDeadlineType(selectedDeadlineType);
+  updateConfList();
 });
